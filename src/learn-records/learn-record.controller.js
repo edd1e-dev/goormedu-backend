a4d204f03268d841ee80be9ee97ca5c7ce87fe44
rojiwon0325 <bond9986@naver.com>
2022-04-06 11:44:46 +0900

    예시 추가

--- src/learn-records/learn-record.controller.js
+++ src/learn-records/learn-record.controller.js
@@ -1,1 +1,12 @@
-export default class LearnRecordController {}
+import LearnRecordService from './learn-records.service';
+
+export default class LearnRecordController {
+  #learnRecordService;
+  constructor() {
+    this.#learnRecordService = new LearnRecordService();
+  }
+
+  async fn() {
+    this.#learnRecordService.findCourseIds();
+  }
+}
