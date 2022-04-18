98e553046b3e86f42f76a676a912437773847d79
ssavassava <sangwo98.kang@gmail.com>
2022-04-18 16:12:02 +0900

    index.ts 수정

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import AppDataSource from './commons/db';
 AppDataSource.initialize()
   .then(() =>
     app.listen(env.PORT, () => {
-      console.log(`✅ Listening on: 'http://${env.DOMAIN}:${env.PORT}`);
+      console.log(`✅ Listening on: '${env.DOMAIN}:${env.PORT}`);
     }),
   )
   .catch((e) => console.log(`❌ DB connection fail: ${e}`));
