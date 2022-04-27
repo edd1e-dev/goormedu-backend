bd9a66e4b38aa79229b0924a3bccb4aa2b93e1eb
ssavassava <sangwo98.kang@gmail.com>
2022-04-27 11:38:17 +0900

    pm2 종료처리 변경

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import env from './commons/config';
 import app from './commons/app';
 import AppDataSource from './commons/db';
+import { createHttpTerminator } from 'http-terminator';
+
+let server;
+let httpTerminator;
 
 process.on('SIGINT', () => {
-  // app.close();
-  process.exit(0);
+  if (server && createHttpTerminator)
+  {
+    setTimeout(() => {
+      httpTerminator.terminate()
+    }, 1000)
+  }
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000)
 });
 
 AppDataSource.initialize()
   .then(() => {
-      app.listen(env.PORT, () => {
+      server = app.listen(env.PORT, () => {
         (<any> process).send('ready');
         console.log(`✅ Listening on: '${env.DOMAIN}:${env.PORT}`);
       })
+      httpTerminator = createHttpTerminator({ server });
     }
   )
   .catch((e) => console.log(`❌ DB connection fail: ${e}`));
